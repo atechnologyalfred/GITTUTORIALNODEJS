29bfa11ee37338bf964c268c6d8843c1a57d2885
atechnology <adjekotaa@gmail.com>
2026-07-23 07:15:49 -0700

    just trying ...

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ import subtract from './subtract.js';
 console.log(add(20, 30));
 console.log(subtract(30, 100))
 console.log("I am learning pull and fetch in git")
+console.log("I am trying the fetch and pull feature again")
